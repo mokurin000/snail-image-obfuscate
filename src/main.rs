ae326c944c92c582157098587e2a4b0200c75cf8
poly000 <1348292515@qq.com>
2021-11-29 05:17:05 +0800

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use std::ops::{Generator, GeneratorState};
 use std::pin::Pin;
 
 extern crate pretty_env_logger;
-#[macro_use] extern crate log;
-use clap::{App,Arg};
+#[macro_use]
+extern crate log;
+use clap::{App, Arg};
 use image::{GenericImageView, ImageFormat};
 
 use image::io::Reader;
 
-fn main() -> Result<(),Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
 
     // return indices in "snail sort", for example,
@@ -29,29 +30,29 @@ fn main() -> Result<(),Box<dyn std::error::Error>>{
         {
             if width == 1 {
                 for (h, w) in (0..height).map(move |i| (off + i, off)) {
-                    yield (h,w);
+                    yield (h, w);
                 }
                 return;
             }
 
             if height == 1 {
-                for (h, w) in (0..width).map(move |i| (off, off + i )) {
-                    yield (h,w);
+                for (h, w) in (0..width).map(move |i| (off, off + i)) {
+                    yield (h, w);
                 }
                 return;
             }
 
             for (h, w) in (0..width - 1).map(move |i| (off, i + off)) {
-                yield (h,w);
+                yield (h, w);
             }
             for (h, w) in (0..height - 1).map(move |i| (off + i, off + width - 1)) {
-                yield (h,w);
+                yield (h, w);
             }
             for (h, w) in (1..width).rev().map(move |i| (off + height - 1, off + i)) {
-                yield (h,w);
+                yield (h, w);
             }
             for (h, w) in (1..height).rev().map(move |i| (i + off, off)) {
-                yield (h,w);
+                yield (h, w);
             }
         }
     };
@@ -69,30 +70,30 @@ fn main() -> Result<(),Box<dyn std::error::Error>>{
         {
             if width == 1 {
                 for (h, w) in (0..height).map(move |i| (off + i, off)).rev() {
-                    yield (h,w);
+                    yield (h, w);
                 }
             } else if height == 1 {
                 for (h, w) in (0..width).map(move |i| (off, off + i)).rev() {
-                    yield (h,w);
+                    yield (h, w);
                 }
             }
 
             for (h, w) in (1..height).map(move |i| (i + off, off)) {
-                yield (h,w);
+                yield (h, w);
             }
 
             for (h, w) in (1..width).map(move |i| (off + height - 1, off + i)) {
-                yield (h,w);
+                yield (h, w);
             }
 
             for (h, w) in (0..height - 1)
                 .map(move |i| (off + i as u32, off + width - 1))
                 .rev()
             {
-                yield (h,w);
+                yield (h, w);
             }
             for (h, w) in (0..width - 1).map(move |i| (off, i + off)).rev() {
-                yield (h,w);
+                yield (h, w);
             }
         }
     };
@@ -101,14 +102,18 @@ fn main() -> Result<(),Box<dyn std::error::Error>>{
         .version("0.1.0")
         .author("poly000 <pedajilao@vip.qq.com>")
         .about("Simple image confuse tool")
-        .arg(Arg::with_name("INPUT")
-            .help("Sets the input file to use")
-            .required(true)
-            .index(1))
-        .arg(Arg::with_name("OUTPUT")
-            .help("Sets the output file to write (*.png)")
-            .required(true)
-            .index(2))
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .help("Sets the output file to write (*.png)")
+                .required(true)
+                .index(2),
+        )
         .get_matches();
 
     let input = matches.value_of("INPUT").unwrap();
@@ -122,24 +127,26 @@ fn main() -> Result<(),Box<dyn std::error::Error>>{
 
     let height = input_img.height();
     let width = input_img.width();
-    info!("height & width: {}, {}",height,width);
+    info!("height & width: {}, {}", height, width);
 
-    let pixels = height*width/2;
+    let pixels = height * width / 2;
+    debug!("pixels: {}", pixels);
     info!("start converting input image to RGBA16");
     let mut mut_img = input_img.to_rgba16();
     for _ in 0..pixels {
-        if let GeneratorState::Yielded((hy,hx)) = Pin::new(&mut snail_sort).resume((height,width)) {
-            if let GeneratorState::Yielded((ty,tx)) = Pin::new(&mut snail_sort_rev).resume((height, width))
+        if let GeneratorState::Yielded((hy, hx)) = Pin::new(&mut snail_sort).resume((height, width))
+        {
+            if let GeneratorState::Yielded((ty, tx)) =
+                Pin::new(&mut snail_sort_rev).resume((height, width))
             {
-                let p = *mut_img.get_pixel(hx,hy);
-                *mut_img.get_pixel_mut(hx,hy) = *mut_img.get_pixel(tx,ty);
-                *mut_img.get_pixel_mut(tx,ty) = p;
+                let p = *mut_img.get_pixel(hx, hy);
+                *mut_img.get_pixel_mut(hx, hy) = *mut_img.get_pixel(tx, ty);
+                *mut_img.get_pixel_mut(tx, ty) = p;
             }
         }
     }
 
-
-    info!("start writing to {}",output);
-    mut_img.save_with_format(output,ImageFormat::Png)?;
+    info!("start writing to {}", output);
+    mut_img.save_with_format(output, ImageFormat::Png)?;
     Ok(())
 }
